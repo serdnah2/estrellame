d503000bc2a8dda3405ff878fe332a5d81cd578e
Juan Naranjo <juandnaranjol@gmail.com>
2019-11-02 10:38:52 -0500

    cambios

--- js/login.js
+++ js/login.js
@@ -3,20 +3,21 @@ window.onload = () => {
     loginForm.addEventListener('submit', event => {
         event.preventDefault();
         const email = document.querySelector('#email-input');
+        const errorMessage = document.querySelector('#login-error');
 
         if (email.value !== '') {
             const emailValid = validateEmail(email.value);
-            const errorMessage = document.querySelector('#login-error');
-
-
             if (emailValid) {
                 //errorMessage.innerHTML = 'El correo registrado no se encuentra registrado, intenta de nuevo';
                 //email.classList.add('has-error');
                 window.location.href = 'home.html'
             } else {
-                errorMessage.innerHTML = 'Por favor ingrese un e-mail válido';
+                errorMessage.innerHTML = 'No se ha ingresado un correo válido';
                 email.classList.add('has-error');
             }
+        } else {
+            errorMessage.innerHTML = 'No se ha ingresado ningún correo';
+            email.classList.add('has-error');
         }
     });
 
